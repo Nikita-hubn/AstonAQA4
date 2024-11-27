032ec240b0a1fde40831fe7157dd98a982207032
Nikita-hubn <73433620+Nikita-hubn@users.noreply.github.com>
2024-11-27 21:22:36 +0300

    e

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,18 +1,23 @@
 package test;
-import org.testng.Assert;
+
 import programm.Main;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class MainTest {
 
-    @org.testng.annotations.Test
-    public void testFactorial() {
-        Assert.assertEquals(Main.factorial(5), 120);
-        Assert.assertEquals(Main.factorial(0), 1);
-        Assert.assertEquals(Main.factorial(1), 1);
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(Main.factorial(0), 1);
     }
 
-    @org.testng.annotations.Test(expectedExceptions = IllegalArgumentException.class)
-    public void testFactorialNegative() {
-        Main.factorial(-1);
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(Main.factorial(4), 120);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        Main.factorial(-54);
     }
 }
